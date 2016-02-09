78200eaebf9c2602b80687ac023bc5954c759973
Yuriy Reshetylo <yuriy.reshetylo@gmail.com>
2016-02-09 15:22:31 +0200

    SDAcademy templates added

--- sdacademy/urls.py
+++ sdacademy/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'sdacademy.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'sdacademy.views.index', name='index'),
+    url(r'^contact/', 'sdacademy.views.contact', name='contact'),
+    url(r'^student_list/', 'sdacademy.views.student_list', name='student_list'),
+    url(r'^student_detail/', 'sdacademy.views.student_detail', name='student_detail'),
 
     url(r'^polls/', include('polls.urls', namespace='polls')),
     url(r'^admin/', include(admin.site.urls)),
