92e0d3aaa5ecb2b5d88fb5034bd02a15efccddcb
Yuriy Reshetylo <yuriy.reshetylo@gmail.com>
2016-02-25 06:48:17 +0200

    Start for 9.2 task

--- students/admin.py
+++ students/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from models import Student
 
 
-admin.site.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    search_fields = ['surname', 'email']
+    list_display = ['name', 'email', 'skype']
+    list_filter = ['courses']
+
+
+admin.site.register(Student, StudentAdmin)
