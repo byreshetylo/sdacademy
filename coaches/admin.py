4cb8f15d606be6d4c216c53f8286a9402ff7d1a8
Yuriy Reshetylo <yuriy.reshetylo@gmail.com>
2016-02-28 12:39:51 +0200

    KZ10 done #2

--- coaches/admin.py
+++ coaches/admin.py
@@ -4,7 +4,9 @@ from models import Coach
 
 class CoachAdmin(admin.ModelAdmin):
     list_display = ['first_name', 'last_name', 'gender', 'skype', 'description']
-    list_filter = ('is_staff', 'is_superuser', 'is_active', 'groups')
+    #list_filter = [is_staff()]
 
+    def is_staff(self):
+        return Coach.is_staff()
 
 admin.site.register(Coach, CoachAdmin)
--- coaches/models.py
+++ coaches/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Coach(User):
+class Coach(models.Model):
     user = models.OneToOneField(User)
     date_of_birth = models.DateField()
     gender = models.CharField(max_length=1, choices=(('M', 'Male'), ('F', 'Female')))
@@ -13,3 +13,15 @@ class Coach(User):
 
     def __unicode__(self):
         return self.user.username
+
+    def first_name(self):
+        return self.user.first_name
+
+    def last_name(self):
+        return self.user.last_name
+
+    def email(self):
+        return self.user.email
+
+    def is_staff(self):
+        return self.user.is_staff
