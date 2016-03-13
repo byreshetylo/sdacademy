cabc2802668d8e8056a5fc00f757995594cccc21
Yuriy Reshetylo <yuriy.reshetylo@gmail.com>
2016-03-13 15:51:32 +0200

    11.2 done

--- quadratic/views.py
+++ quadratic/views.py
@@ -7,22 +7,12 @@ from math import sqrt, fabs
 
 def quadratic_results(request):
     form = QuadraticForm(request.GET)
-    a_data = check_value(request.GET.get('a', ''), True)
-    b_data = check_value(request.GET.get('b', ''))
-    c_data = check_value(request.GET.get('c', ''))
-    if a_data['error'] == '' and b_data['error'] == '' and c_data['error'] == '':
-        d_data = d_calculate(a_data['value'], b_data['value'], c_data['value'])
-    else:
-        d_data = {'d': '', 'd_desc': ''}
+    d_data = {'d': '', 'd_desc': ''}
+    if form.is_valid():
+        d_data = d_calculate(form.cleaned_data['a'], form.cleaned_data['b'], form.cleaned_data['c'])
     context = {
-        'a_value': a_data['value'],
-        'b_value': b_data['value'],
-        'c_value': c_data['value'],
         'd_value': d_data['d'],
         'd_details': d_data['d_desc'],
-        'a_error': a_data['error'],
-        'b_error': b_data['error'],
-        'c_error': c_data['error'],
         'form': form,
     }
     return render(request, 'results.html', context)
@@ -41,24 +31,3 @@ def d_calculate(a, b, c):
     else:
         d_desc = u'Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s' % (x1, x2)
     return {'d': d, 'd_desc': d_desc}
-
-
-def check_value(in_value, first=False):
-    error = ''
-
-    if in_value == '':
-        value = in_value
-        error = u'коэффициент не определен'
-    else:
-        try:
-            value = int(in_value)
-        except ValueError:
-            value = in_value
-            error = u'коэффициент не целое число'
-            return {'value': value, 'error': error}
-
-        if value == 0 and first is True:
-            error = u'коэффициент при первом слагаемом уравнения не может быть равным нулю'
-
-    return {'value': value, 'error': error}
-
